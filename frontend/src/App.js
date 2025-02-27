fdd61d683533fc391c2e0eeccbd4fcec06d9cc25
Edward-Zn <edward.zoravarian@gmail.com>
2025-02-27 17:08:42 +0100

    App.js. React components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,35 @@
-import { useEffect, useState } from "react";
+import React, { useState } from "react";
+import PlayerForm from "./components/PlayerForm";
+import GameBoard from "./components/GameBoard";
+import TurnIndicator from "./components/TurnIndicator";
+import ActionPanel from "./components/ActionPanel";
 
 function App() {
-  const [message, setMessage] = useState("");
+  const [players, setPlayers] = useState([]);
+  const [currentTurn, setCurrentTurn] = useState(0);
 
-  useEffect(() => {
-    fetch("http://localhost:5000")
-      .then(res => res.text())
-      .then(data => setMessage(data));
-  }, []);
+  const handlePlayerSubmit = (playerName) => {
+    setPlayers([...players, { name: playerName, id: players.length }]);
+  };
 
-  return <div>{message}</div>;
+  const handleEndTurn = () => {
+    setCurrentTurn((prev) => (prev + 1) % players.length);
+  };
+
+  return (
+    <div className="container mx-auto p-4 text-center">
+      <h1 className="text-3xl font-bold mb-4">Sunflower RPG Battle</h1>
+      {players.length < 2 ? (
+        <PlayerForm onSubmit={handlePlayerSubmit} />
+      ) : (
+        <>
+          <TurnIndicator currentPlayer={players[currentTurn]} />
+          <GameBoard players={players} />
+          <ActionPanel onEndTurn={handleEndTurn} />
+        </>
+      )}
+    </div>
+  );
 }
 
 export default App;
